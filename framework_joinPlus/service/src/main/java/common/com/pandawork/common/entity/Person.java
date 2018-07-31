7f845074f3a6aa0e9c6dcb24cff59be27ba552b6
xiaoy990 <xiaoy990@nenu.edu.cn>
2018-07-31 17:51:04 +0800

    xiaoyao

--- framework_joinPlus/service/src/main/java/common/com/pandawork/common/entity/Person.java
+++ framework_joinPlus/service/src/main/java/common/com/pandawork/common/entity/Person.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * @Author:liangll lianglltest
+ * @Author:liangll lianglltest xiaoyao
  * @Description: person实体
  * @Date: 15:39 2018/7/28
  */
